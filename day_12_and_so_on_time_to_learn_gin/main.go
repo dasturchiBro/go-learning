9687d9eb0f338d7b4b74931d0f2a305d99bf156f
Fayozbek Erkinjonov <fayozbek.online@gmail.com>
2025-10-30 21:53:13 +0500

    Day 12: Started learning Gin

---
+++ day_12_and_so_on_time_to_learn_gin/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/home", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "What's up?",
+		})
+	})
+
+	r.Run()		
+}
