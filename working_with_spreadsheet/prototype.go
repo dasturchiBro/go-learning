aa04e414c5e3e3a80c677dae0a326ae4a73d3be7
Fayozbek Erkinjonov <fayozbek.online@gmail.com>
2025-12-08 23:45:37 +0500

    Working with Spreadsheets

--- working_with_spreadsheet/prototype.go
+++ working_with_spreadsheet/prototype.go
@@ -10,6 +10,9 @@ import (
 	"os"
 )
 
+var reader = bufio.NewReader(os.Stdin)
+
+
 func Check(err error) {
 	if err != nil {
 		log.Fatal(err)
@@ -17,15 +20,14 @@ func Check(err error) {
 }
 
 func Header() string {
-	var class, quarter, examType string
 	fmt.Println("Enter the class: ")
-	fmt.Scanln(&class)
+	class, _ := reader.ReadString('\n')
 
 	fmt.Println("Enter the quarter(chorak): ")
-	fmt.Scanln(&quarter)
+	quarter, _ := reader.ReadString('\n')
 
 	fmt.Println("Enter the exam type(e.g 1-BSB, CHSB, 2-BSB, etc.): ")
-	fmt.Scanln(&examType)
+	examType, _ := reader.ReadString('\n')
 	header := fmt.Sprintf("POP tuman IM %v-sinf o'quvchilarining %v-chorak %v natijalari", class, quarter, examType)
 	return header
 }
@@ -34,15 +36,17 @@ func SetCriteria() map[string]int {
 	criteria := make(map[string]int) 
 	for {
 		fmt.Print("How many criteria(Vocabulary 5, Writing 8, Grammar 9, etc.) do you want to add? \nEnter the number:  ")
-		var numberOfCriteria int
-		fmt.Scanln(&numberOfCriteria)
+		numberOfCriteria_string, _ := reader.ReadString('\n')
+		numberOfCriteria_string = strings.TrimSpace(numberOfCriteria_string)
+		numberOfCriteria, err := strconv.Atoi(numberOfCriteria_string)
+		Check(err)
 		fmt.Println()
 		if numberOfCriteria > 6 {
 			fmt.Println("You can add up to 6 criteria. No more.\nPlease try again.")
 			continue
 		}
 		for i := range numberOfCriteria {
-			reader := bufio.NewReader(os.Stdin)
+			
 			fmt.Print(strconv.Itoa(i+1)+") Please enter a criterion in this form: Criterion Point (e.g Writing 9).\nEnter: ")
 			input, _ := reader.ReadString('\n')
 			input = strings.TrimSpace(input)
@@ -57,6 +61,90 @@ func SetCriteria() map[string]int {
 		break
 	}
 	return criteria
+}
+
+func AddStudents(f *excelize.File) {
+	message := "How many students do you want to add? (enter in numbers)\nEnter: "
+	fmt.Print(message)
+	
+	numberOfStudents, _ := reader.ReadString('\n')
+	fmt.Println()
+	number, err := strconv.Atoi(strings.TrimSpace(numberOfStudents))
+	Check(err)
+	for i := range number {
+		fmt.Printf("Enter the name of the student on number %d: ", i+1)
+		input, _ := reader.ReadString('\n')
+		rowNum := strconv.Itoa(i+6)
+		f.SetCellValue("Sheet1", "A"+rowNum, i+1)
+		f.SetCellValue("Sheet1", "B"+rowNum, input)
+	}
+	fmt.Print("Do you want to add scores to the students you added? \nY/N:")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	switch input {
+	case "Y", "y", "YES", "yes":
+		AddScoresToStudents(f)
+	default:
+		return
+	}
+}
+func AddScoresToStudents(f *excelize.File) {
+	start_index := 6
+
+	for {
+		indexOfRow := strconv.Itoa(start_index)
+		cell, err := f.GetCellValue("Sheet1", "B"+indexOfRow)
+		Check(err)
+		cell = strings.TrimSpace(cell)
+		if cell == "" {
+			return
+		}
+		fmt.Printf("What scores would you like to add to %s?\nEnter (in this form: 10 6 9 ...): ", cell)
+		scores, _ := reader.ReadString('\n')
+		scores = strings.TrimSpace(scores)
+		parts := strings.Split(scores, " ")
+		letters := []string{"C", "D", "E", "F", "G", "H", "I", "J"}
+		total := 0
+		for part := range len(parts) {
+			Check(f.SetCellValue("Sheet1", letters[part]+indexOfRow, parts[part]))
+			num, err := strconv.Atoi(parts[part])
+			Check(err)
+			total += num
+		}
+		totalVal, err := f.GetCellValue("Sheet1", letters[len(parts)]+"5")
+		Check(err)
+		totalVal = strings.TrimSpace(totalVal)
+		totalValue := strings.Split(totalVal, " ")[1]
+		totalValue_int, err := strconv.Atoi(totalValue)
+		Check(err)
+		fmt.Println(totalValue_int)
+		percent := fmt.Sprintf("%.2f", (float64(total)/float64(totalValue_int)) * 100)
+		Check(f.SetCellValue("Sheet1", letters[len(parts)+1]+indexOfRow, (percent)))
+		Check(f.SetCellValue("Sheet1", letters[len(parts)]+indexOfRow, total))
+		start_index += 1
+	}
+}
+func AddScoresToStudent(f *excelize.File) {}
+
+func Body(f *excelize.File) {
+	methods := "Choose one of these methods: \n1) Add students\n2) Add scores to a student\n3) Add scores to students\n0) Exit\n"
+	for {
+		fmt.Println(methods)
+		answer, _ := reader.ReadString('\n')
+		answer = strings.TrimSpace(answer)
+		switch answer {
+		case "1":
+			AddStudents(f)
+		case "2":
+			AddScoresToStudent(f)
+		case "3":
+			AddScoresToStudents(f)
+		case "0":
+			return
+		default:
+			fmt.Println("Undefined method. Try again. Method you typed: ", answer)
+		}
+	}
 }
 
 func main() {
@@ -82,6 +170,11 @@ func main() {
 	Check(f.SetCellValue("Sheet1", letters[index+2]+"5", "Foiz"))
 	Check(f.MergeCell("Sheet1", "A1", letters[index+2]+"4"))
 	// End Set Up Header
+
+
+	// START Body
+	Body(f)
+	// END Body
 
 
 	// Style Start
